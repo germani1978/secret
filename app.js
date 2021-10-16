ab9ea29a481e33ada4c1577034be2c91f24edb23
Germani <germanibenitezalfonso1@gmail.com>
2021-10-16 19:00:48 -0400

    bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { stringify } = require("qs");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -21,13 +22,11 @@ const userShema = new mongoose.Schema({
 });
 
 
-userShema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:['password'] });
-
 const User = new mongoose.model("User",userShema);
 
 app.get('/', function (req, res){
     res.render('home');
-});
+}); 
 
 app.get('/login', function (req, res){
     res.render('login');
@@ -38,18 +37,24 @@ app.get('/register', function (req, res){
 });
 
 app.post('/register', function (req, res){
-    const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            }else {
+                res.render("secrets");
+            }
+        });
     });
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        }else {
-            res.render("secrets");
-        }
-    });
+    
 });
 
 app.post('/login', function (req, res){
@@ -58,21 +63,21 @@ app.post('/login', function (req, res){
     const password = req.body.password;
 
     User.findOne({email: username} , function (err, foundUser){
-        if (err) console.log(err);
-        else { 
+        if (err) {
+             console.log(err);
+        } else { 
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            } else {  
-                console.log("No estas en el registro") 
-                res.redirect("/login");
-            }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                }); 
+            }    
+
         }
     });
 });
-
-
 
 app.listen(3000,function () {
     console.log('Server started on port 3000');
